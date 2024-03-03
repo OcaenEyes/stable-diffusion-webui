5f909dccee225b37b3eb8ec8080adda489169613
OcaenEyes <1450136519@qq.com>
2024-03-03 21:20:22 +0800

    update

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:/Python310/python.exe"
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set VENV_DIR=C:/Users/Administrator/Envs/sdenv3.10.6
+set COMMANDLINE_ARGS=--xformers --api
 
 call webui.bat
